9ee8f9ccca6c5f72fd5d50d72ecc583859b86a58
Hans Vos <hans.vos@codeimmersives.com>
2019-10-28 14:26:17 -0400

    first commit

--- main.js
+++ main.js
@@ -1,43 +1,119 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num, num1) {
+
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  return (num**2) / 2;
 }
 
-function isLong() {
+function isLong(str) {  
+  const strLength = str.length;
+
+  if(strLength => 15) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function exclaim() {
+function exclaim(str) {
+  let newStr = '';
+
+  for(let i = 0; i < str.length; i++) {
+      newStr = str + '!';
+    }
+
+  return newStr;
 }
 
-function countWords() {
+function countWords(str) {
+  let strSplit = str.split(' ');
+  return strSplit.length;
 }
 
 function containsDigit() {
+
 }
 
-function containsLowerCase() {
+function containsLowerCase(str) {
+  let strSplit = str.split('');
+  console.log(strSplit)
+  for(let i = 0; i <strSplit.length; i++){
+    if(strSplit[i] === strSplit.LowerCase) {
+      return true;
+    } else {
+      return false
+    }
+  }
 }
 
 function containsUpperCase() {
+  let strSplit = str.split('');
+  console.log(strSplit)
+  for(let i = 0; i <strSplit.length; i++){
+    if(strSplit[i] === strSplit.UpperCase) {
+      return true;
+    } else {
+      return false
+    }
+  }
 }
 
 function containsNonAlphanumeric() {
+
 }
 
-function containsSpace() {
+function containsSpace(str) {
+  let strSplit = str.split('');
+  console.log(strSplit);
+  for(let i = 0; i < strSplit.length; i++) {
+    if(strSplit[i] === ' ') {
+      return true
+    } 
+  }
+  return false
 }
 
-function digits() {
+function digits(num) {
+  let splitDigits = [];
+
+  const numToString = num.toString();
+  console.log(numToString)
+  const numStringSplit = numToString.split('');
+  console.log(numStringSplit);
+  for (let i = 0; i < numStringSplit.length; i++){
+    splitDigits.push(Number(numStringSplit[i]));
+  }
+
+  return splitDigits;
 }
 
-function truncate() {
-}
+function truncate(str) {
+  let newStr = '';
+  const first8 = str.slice(0, 8) + '...';
+
+  if(str.length >= 15){
+    newStr = newStr + first8;
+    } else {
+    newStr = newStr + str;
+    }
+
+  return newStr;
+  }
 
 function isValidPassword() {
+
 }
 
-function onlyPunchy() {
+function onlyPunchy(arr) {
+  let newArray = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].length < 15){
+      newArray.push(arr[i])
+    }
+  }
+  return newArray;
 }
 
 
